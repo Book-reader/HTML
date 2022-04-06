c853876be84c79ced920a13963dd21a54a71779d
Book-reader <89329785+Book-reader@users.noreply.github.com>
2022-04-06 14:27:01 +1200

    Finished The Quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,11 @@
 var questionNum = 1;
 var correct = false;
 var questionVal;
-var questionText = ["What is the current date? (dd/mm/yyyy)","What is 23.5 ÷ 4?","sample text","sample text","sample text"]
+var questionText = ["What is the current date? (dd/mm/yyyy)","What is New Zealands national animal?","What does 你好 mean?","sample text","sample text"]
 var date = new Date();
 var now = date.toLocaleDateString('en-NZ');
-
+var score = 0
+var canChangeScore = true;
 
 window.onload = function() {
     var question = ("Question "+questionNum +": "+questionText[(questionNum-1)]);
@@ -14,74 +15,80 @@ window.onload = function() {
 
 function checkAnswer(){
     questionVal = document.getElementById("question").value;
-
+    
     if(questionNum == 1){
         question1()
     }else if(questionNum ==2){
         question2()
     }else if(questionNum ==3){
         question3()
-    }else if(questionNum ==4){
-        question4()
-    }else if(questionNum ==5){
-        question5()
     }
-
 }
 
 function reload(){
     if(correct==false){
         document.location.reload();
-    }else{
+    }else if(questionNum<3){
+        questionNum+=1;
         var question = ("Question "+questionNum +": "+questionText[(questionNum-1)]);
         document.getElementById('questionNum').innerHTML = question;
         document.getElementById('question').value = "";
         document.getElementById('answer').innerHTML = "";
         document.getElementById('next').value = "Restart Quiz";
         correct=false;
+        canChangeScore=true;
+    }else{
+        alert("Well Done, your final score is "+score);
     }
 }
 
 function question1(){
     if (/*questionNum == 1 && */questionVal == now || questionVal == 0+now) {
-        questionNum = 2;
-        console.log("correct");
-        document.getElementById('answer').innerHTML = "Correct";
-        correct = true;
-        document.getElementById('next').value = "Next Question";
+        isCorrect();
     }else /*if (questionNum == 1) */{
-        console.log("incorrect");
-        questionNum = 1;
-        document.getElementById('answer').innerHTML = "Incorrect";
-        correct = false;
-        document.getElementById('next').value = "Restart Quiz";
+        incorrect();
     }
 }
 
 function question2(){
-    console.log(23.5 / 4)
-    if(document.getElementById('question').value == (23.5 / 4)){
-        questionNum = 3;
-        document.getElementById('answer').innerHTML = "Correct";
-        correct = true;
-        document.getElementById('next').value = "Next Question";
+    if(questionVal.toLowerCase()=="kiwi"){
+        isCorrect();
     }else{
-        console.log("incorrect");
-        questionNum = 2;
-        document.getElementById('answer').innerHTML = "Incorrect";
-        correct = false;
-        document.getElementById('next').value = "Restart Quiz";
+        incorrect();
     }
 }
 
 function question3(){
-
+    if(questionVal.toLowerCase()=="hi"||questionVal.toLowerCase()=="hello"){
+        isCorrect();
+        questionNum = 3;
+        document.getElementById('next').value = "Finish";
+    }else{
+        incorrect();
+    }
 }
 
-function question4(){
 
+function isCorrect(){
+    console.log("correct");
+    document.getElementById('answer').innerHTML = "Correct";
+    correct = true;
+    document.getElementById('next').value = "Next Question";
+    if(canChangeScore==true){
+        score = score+5;
+        document.getElementById('score').innerHTML = "Score: "+score;
+        canChangeScore=false;
+    }
 }
 
-function question5(){
-
+function incorrect(){
+    console.log("incorrect");
+    document.getElementById('answer').innerHTML = "Incorrect";
+    correct = false;
+    document.getElementById('next').value = "Restart Quiz";
+    if(canChangeScore==true){
+        score = score-5;
+        document.getElementById('score').innerHTML = "Score: "+score;
+        canChangeScore=false;
+    }
 }
